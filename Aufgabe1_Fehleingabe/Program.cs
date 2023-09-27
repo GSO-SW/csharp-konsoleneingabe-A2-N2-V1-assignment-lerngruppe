f8e3f62cf1f94665599f27c6d56008ed8f1a1333
AI-Assistant <k.akinci@gso.schule.koeln>
2023-09-27 07:35:23 +0200

    Test

--- Aufgabe1_Fehleingabe/Program.cs
+++ Aufgabe1_Fehleingabe/Program.cs
@@ -14,9 +14,41 @@ public static class Aufgabe_1
 {
     public static (string, string, short, double, bool) Aufgabe1()
     {
-        
-        //Diese Zeile mit Ihrem Quellcode ersetzen
+        string vorname = "";
+        string nachname = "";
+        short alter = 0;
+        double groesse = 0;
+        bool erste_ps=false;
 
+        Console.WriteLine("Persönliche Daten");
+
+        try
+        {
+
+            Console.WriteLine("Geben Sie Ihren Vornamen ein:");
+            vorname = Console.ReadLine();
+            Console.WriteLine("Geben Sie Ihren Nachnamen ein:");
+            nachname = Console.ReadLine();
+
+            Console.WriteLine("Wie alt sind Sie?:");
+            alter = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Wie groß sind Sie?:");
+            groesse = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ist C# Ihre erste Programmiersprache?(true/false):");
+            erste_ps = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine($"{"Vorname:",-10}{vorname}");
+            Console.WriteLine($"{"Nachname:",-10}{nachname}");
+            Console.WriteLine($"{"Alter:",-10}{alter}");
+            Console.WriteLine($"{"Größe:",-10}{groesse}");
+            Console.WriteLine($"{"ProgErf:",-10}{erste_ps}");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Falsche Eingabe!");
+        }
         return (vorname, nachname, alter, groesse, erste_ps);
 
     }
